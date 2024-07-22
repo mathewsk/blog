17485da2e3e9a4eceb0a1daeac95c985d1538e95
Mat <matashmail-github@yahoo.com>
2024-07-21 19:58:56 -0500

    changed environment.ts baseUrl , production, apiUrl, websiteUrl since runtime was throwing error saying couldnt find url

--- blog/src/environments/environment.ts
+++ blog/src/environments/environment.ts
@@ -1,6 +1,11 @@
 export const environment = {
-  baseUrl: import.meta.env['VITE_BASE_URL'],
-  production: import.meta.env['VITE_PRODUCTION'] === 'true',
-  apiUrl: import.meta.env['VITE_API_URL'],
-  websiteUrl: import.meta.env['VITE_WEBSITE_URL'],
+  // baseUrl: import.meta.env['VITE_BASE_URL'],
+  // production: import.meta.env['VITE_PRODUCTION'] === 'true',
+  // apiUrl: import.meta.env['VITE_API_URL'],
+  // websiteUrl: import.meta.env['VITE_WEBSITE_URL'],
+
+  baseUrl: 'http://localhost:4200',
+  production: 'http://localhost:4200',
+  apiUrl: 'http://localhost:4200',
+  websiteUrl: 'http://localhost:4200'
 };
